a267cde00337dc602c8d7aef8af02e4e3174ab61
bagas74 <bagassatrio744@gmail.com>
2025-06-04 10:35:29 +0700

    menambahkan product untuk home

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,1 +1,101 @@
+class Product {
+  final String name;
+  final String author;
+  final String genre;
+  final double price;
+  final String image;
 
+  Product({
+    required this.name,
+    required this.author,
+    required this.genre,
+    required this.price,
+    required this.image,
+  });
+}
+
+final demoProducts = [
+  Product(
+    name: "Fatherhood",
+    author: "Marcus Berkmann",
+    genre: "Parenting",
+    price: 4.99,
+    image: 'assets/images/bumi1.jpg',
+  ),
+  Product(
+    name: "Fatherhood",
+    author: "Marcus Berkmann",
+    genre: "Parenting",
+    price: 4.99,
+    image: 'assets/images/bumi1.jpg',
+  ),
+  Product(
+    name: "Fatherhood",
+    author: "Marcus Berkmann",
+    genre: "Parenting",
+    price: 4.99,
+    image: 'assets/images/bumi1.jpg',
+  ),
+  Product(
+    name: "Fatherhood",
+    author: "Marcus Berkmann",
+    genre: "Parenting",
+    price: 4.99,
+    image: 'assets/images/bumi1.jpg',
+  ),
+  Product(
+    name: "Fatherhood",
+    author: "Marcus Berkmann",
+    genre: "Parenting",
+    price: 4.99,
+    image: 'assets/images/bumi1.jpg',
+  ),
+  // Tambahkan data lainnya...
+];
+
+final newProducts = [
+  Product(
+    name: "Bumi",
+    author: "Tere Liye",
+    genre: "Fiksi",
+    price: 4.99,
+    image: 'assets/images/bumi1.jpg',
+  ),
+  Product(
+    name: "Fatherhood",
+    author: "Marcus Berkmann",
+    genre: "Parenting",
+    price: 4.99,
+    image: 'assets/images/bumi1.jpg',
+  ),
+  Product(
+    name: "Fatherhood",
+    author: "Marcus Berkmann",
+    genre: "Parenting",
+    price: 4.99,
+    image: 'assets/images/bumi1.jpg',
+  ),
+  Product(
+    name: "Fatherhood",
+    author: "Marcus Berkmann",
+    genre: "Parenting",
+    price: 4.99,
+    image: 'assets/images/bumi1.jpg',
+  ),
+  Product(
+    name: "Fatherhood",
+    author: "Marcus Berkmann",
+    genre: "Parenting",
+    price: 4.99,
+    image: 'assets/images/bumi1.jpg',
+  ),
+  Product(
+    name: "Fatherhood",
+    author: "Marcus Berkmann",
+    genre: "Parenting",
+    price: 4.99,
+    image: 'assets/images/bumi1.jpg',
+  ),
+
+  // Tambahkan data lainnya...
+];
--- lib/screens/product_detail.dart
+++ lib/screens/product_detail.dart
@@ -1,1 +1,42 @@
+import 'package:flutter/material.dart';
+import '../../models/product.dart';
 
+class ProductDetail extends StatelessWidget {
+  final Product product;
+  const ProductDetail({super.key, required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: BackButton(),
+        backgroundColor: Colors.white,
+        elevation: 0,
+      ),
+      body: Column(
+        children: [
+          Image.asset(product.image, height: 200),
+          const SizedBox(height: 16),
+          Text(
+            product.name,
+            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 8),
+          Text('\$${product.price.toStringAsFixed(2)}'),
+          const SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green,
+                minimumSize: const Size(double.infinity, 50),
+              ),
+              child: const Text("Add To Basket"),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
