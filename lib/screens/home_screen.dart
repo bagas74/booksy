b57e10422a3adee7049d8845f580a9f851635a06
bagas74 <bagassatrio744@gmail.com>
2025-06-04 10:33:02 +0700

    membuat card

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../models/product.dart';
+import 'product_detail.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -60,12 +62,110 @@ class _HomeScreenState extends State<HomeScreen> {
                   scrollDirection: Axis.horizontal,
                   shrinkWrap: true,
                   physics: const BouncingScrollPhysics(),
+                  // children:
+                  //       trendingProducts
+                  //       .map((products) => productCard(context, product))
+                  //       toList(),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Text(
+                    'Terbaru',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  Text('See all', style: TextStyle(color: Colors.green)),
+                ],
+              ),
+              const SizedBox(height: 12),
+              SizedBox(
+                height: 340,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  shrinkWrap: true,
+                  physics: const BouncingScrollPhysics(),
+                  // children:
+                  //       newProducts
+                  //       .map((products) => productCard(context, product))
+                  //       toList(),
                 ),
               ),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  Widget productCard(BuildContext context, Product product) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        // Kartu dengan gambar dan shadow
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => ProductDetail(product: product),
+              ),
+            );
+          },
+          child: Container(
+            width: 140,
+            height: 203,
+            margin: const EdgeInsets.only(right: 16),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black12,
+                  blurRadius: 6,
+                  offset: Offset(0, 4),
+                ),
+              ],
+            ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(16),
+              child: Image.asset(product.image, fit: BoxFit.cover),
+            ),
+          ),
+        ),
+
+        // Text di luar gambar (bukan bagian dari kartu)
+        const SizedBox(height: 8),
+        SizedBox(
+          width: 140, // Samakan dengan lebar gambar
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                product.name,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 14,
+                ),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+              Text(
+                product.author,
+                style: const TextStyle(fontSize: 12, color: Colors.black54),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+              Text(
+                product.genre,
+                style: const TextStyle(fontSize: 12, color: Colors.black38),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+              const SizedBox(height: 10),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
