96cae662163224fc158ebdb088eed7504c63c246
bagas74 <bagassatrio744@gmail.com>
2025-06-04 10:06:36 +0700

    menambahkan fitur search

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -22,11 +22,45 @@ class _HomeScreenState extends State<HomeScreen> {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: const [
                   Text(
-                    "\u{1F4DA} Selamat Membaca",
+                    "\u{1F4DA} Selamat Membaca di booksy",
                     style: TextStyle(fontWeight: FontWeight.bold),
                   ),
                   Icon(Icons.notifications_none),
                 ],
+              ),
+              const SizedBox(height: 16),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Cari Buku',
+                  prefixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(12),
+                child: Image.asset(''),
+              ),
+              const SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Text(
+                    'Trending',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16),
+              SizedBox(
+                height: 200,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  shrinkWrap: true,
+                  physics: const BouncingScrollPhysics(),
+                ),
               ),
             ],
           ),
