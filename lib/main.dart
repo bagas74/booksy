2350514e0342344ba8715982a12ab2f2e8ff2ab7
bagas74 <bagassatrio744@gmail.com>
2025-06-16 12:07:27 +0700

    /,

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,1 @@
-import 'package:booksy/screens/login_screen.dart';
-import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Booksy',
-      home: const LoginScreen(),
-    );
-  }
-}
