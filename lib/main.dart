8f39d481e99a6c38955def5ab69c62358a1c6276
bagas74 <bagassatrio744@gmail.com>
2025-06-17 19:12:35 +0700

    1

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,1 @@
-import 'screens/splash_screen.dart';
-import 'package:flutter/material.dart' show BuildContext, MaterialApp, StatelessWidget, Widget, runApp;
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(title: 'Booksy', home: const SplashScreen());
-  }
-}
